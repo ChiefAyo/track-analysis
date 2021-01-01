815d9c6dd6b41e0ab58153dd42511aa9b724de43
Ayo <aolag00@gmail.com>
2021-01-01 03:20:17 +0000

    css changes and changed LandingPage to class component

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './../App.css';
 
-function LandingPage() {
-    return (
-        <div>Logged in to system</div>
-    );
+class LandingPage extends Component {
+    constructor() {
+        super();
+        
+    }
+
+
+    render() {
+        return (
+            <div>Logged in to system</div>
+        );
+    }
 }
 
 export default LandingPage;
